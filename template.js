42ba904ef7bd4a8190d594132bc20b4cc747539f
omeraydogdu <aydogduomer950@gmail.com>
2024-09-16 21:10:13 +0300

    nan,undefined,typeof...

--- template.js
+++ template.js
@@ -97,40 +97,86 @@ multi Comment
 // alert(userdata);
 /////////////////////////////////////////////////////////////////////////////////////////
 //MATH
-console.log(Math.PI);
-console.log(Math.E);
-console.log(Math.sqrt(25));
-console.log(Math.pow(5,2));
-console.log(5**2);
-console.log(Math.abs(-5));
-console.log(Math.min(-5,5,78,645,1,6));
-console.log(Math.max(-5,5,78,645,1,6));
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.sqrt(25));
+// console.log(Math.pow(5,2));
+// console.log(5**2);
+// console.log(Math.abs(-5));
+// console.log(Math.min(-5,5,78,645,1,6));
+// console.log(Math.max(-5,5,78,645,1,6));
 
-console.log(Math.floor(2.9));   //2
-console.log(Math.ceil(2.6));    //3
-console.log(Math.round(4.5));   //5
-console.log(Math.round(4.6));   //5
-console.log(Math.round(4.4));   //4
+// console.log(Math.floor(2.9));   //2
+// console.log(Math.ceil(2.6));    //3
+// console.log(Math.round(4.5));   //5
+// console.log(Math.round(4.6));   //5
+// console.log(Math.round(4.4));   //4
 
-console.log(Math.sin(8.5));
-console.log(Math.cos(8.5));
-console.log(Math.asin(-1));
-console.log(Math.acos(-1));
-console.log(Math.tan(-1));
-console.log(1/Math.tan(-1));
+// console.log(Math.sin(8.5));
+// console.log(Math.cos(8.5));
+// console.log(Math.asin(-1));
+// console.log(Math.acos(-1));
+// console.log(Math.tan(-1));
+// console.log(1/Math.tan(-1));
 
-console.log(Math.random()*1+10);
-console.log(Math.random(1)+10);
-console.log(Math.random()*10+1);
+// console.log(Math.random()*1+10);
+// console.log(Math.random(1)+10);
+// console.log(Math.random()*10+1);
 
-console.log(Math.floor(Math.random()*10+1));  
+// console.log(Math.floor(Math.random()*10+1));  
 
 
 // ESCAPE CHARACTERS
-console.log("Görüntü Oluşturucu \n uygulamasını açın ve istenirse \nMicrosoft hesabınızla oturum açın."); 
+// console.log("Görüntü Oluşturucu \n uygulamasını açın ve istenirse \nMicrosoft hesabınızla oturum açın."); 
 
-console.log("Görüntü Oluşturucu \r uygulamasını açın ve istenirse \r Microsoft hesabınızla oturum açın."); 
+// console.log("Görüntü Oluşturucu \r uygulamasını açın ve istenirse \r Microsoft hesabınızla oturum açın."); 
 
-console.log("Görüntü Oluşturucu \t uygulamasını açın ve istenirse \t Microsoft hesabınızla oturum açın.");
+// console.log("Görüntü Oluşturucu \t uygulamasını açın ve istenirse \t Microsoft hesabınızla oturum açın.");
  
-console.log("Görüntü Oluşturucu \" uygulamasını açın ve istenirse \" Microsoft hesabınızla oturum açın."); 
+// console.log("Görüntü Oluşturucu \" uygulamasını açın ve istenirse \" Microsoft hesabınızla oturum açın."); 
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Typeof
+let number=8;
+let name="ali";
+let islogin=true;
+console.log(typeof number);    //number
+console.log(typeof name);      //string
+console.log(typeof islogin);   //boolean
+
+// Instanceof
+let objectIstance= new Number(45);
+console.log(objectIstance instanceof Number);   //TRUE çıktısını verir.
+
+// New
+let dataNew=new Number(45);
+console.log(dataNew);  //işaretli bir number var .
+
+dataNew=null;
+console.log(dataNew); //null
+
+// Undefined
+let dataNumber;
+console.log(dataNumber);    //Undefined
+
+// NaN
+let result = 10 /10;
+console.log(result);
+
+let result2 = 10 / "10";
+console.log(result2);
+
+let result3 = 10 /"ABSFS";  //NaN
+console.log(result3);
+
+
+//isNaN = true dönüyosa sayı değildir.
+let result4 = 10 /"ABSFS";
+console.log(!isNaN(result4));    //true
+
+//hoisting
+dataNumber2=55;
+let dataNumber2;
+console.log(dataNumber2);
