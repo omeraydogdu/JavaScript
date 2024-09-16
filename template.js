000292b54b0663789e2e9b92bca164c7e6b2d94e
omeraydogdu <aydogduomer950@gmail.com>
2024-09-16 20:02:53 +0300

    variable

--- template.js
+++ template.js
@@ -14,19 +14,48 @@ multi Comment
 
 //window.
 //document
-console.log("merhabalar");
-//log
-window.console.log("windove merhabası");
-//wanings
-console.warn("console.warn");
+// console.log("merhabalar");
+// //log
+// window.console.log("windove merhabası");
+// //wanings
+// console.warn("console.warn");
 
-//info
-console.warn("bilgiler");
+// //info
+// console.warn("bilgiler");
 
-//error
-console.error("console error");
+// //error
+// console.error("console error");
 
 //alert
 //alert("alert");
 
 /////////////////////////////////////////////////////////////////////////////////////////
+
+// Varıable
+// var , let , const
+// Commen Rules
+var comnpanyName="isim";
+console.log(comnpanyName);
+
+var comnpanyName="isim";
+console.log(comnpanyName);      
+
+var comnpanyName4="isim";
+console.log(comnpanyName4);
+
+var _comnpanyName="isim";  //underscore
+console.log(_comnpanyName);
+
+var $_comnpanyName="isim";  
+console.log($_comnpanyName);
+
+//let
+let companyName2="hello world";
+console.log(companyName2);
+console.warn(companyName2);
+
+
+//const
+const comnpanyName3="hello";
+//comnpanyName3="hello22";  değişmez
+console.error(comnpanyName3);
