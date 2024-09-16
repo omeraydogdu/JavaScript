779eaa70d1ff9f5b3d0e307e7670b93bdf11fd30
omeraydogdu <aydogduomer950@gmail.com>
2024-09-16 19:03:18 +0300

    console

--- template.js
+++ template.js
@@ -1,1 +1,32 @@
-console.log("external js")
+//console.log("external js")
+
+
+
+// Single Comment
+/*
+multi Comment
+multi Comment
+multi Comment
+*/
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+//window.
+//document
+console.log("merhabalar");
+//log
+window.console.log("windove merhabası");
+//wanings
+console.warn("console.warn");
+
+//info
+console.warn("bilgiler");
+
+//error
+console.error("console error");
+
+//alert
+//alert("alert");
+
+/////////////////////////////////////////////////////////////////////////////////////////
