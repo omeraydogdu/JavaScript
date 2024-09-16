9a4f4ee8491c465de36895ffc3ebd91d7804e8e5
omeraydogdu <aydogduomer950@gmail.com>
2024-09-16 20:19:29 +0300

    operators , prompt

--- template.js
+++ template.js
@@ -34,28 +34,65 @@ multi Comment
 // Varıable
 // var , let , const
 // Commen Rules
-var comnpanyName="isim";
-console.log(comnpanyName);
+// var comnpanyName="isim";
+// console.log(comnpanyName);
 
-var comnpanyName="isim";
-console.log(comnpanyName);      
+// var comnpanyName="isim";
+// console.log(comnpanyName);      
 
-var comnpanyName4="isim";
-console.log(comnpanyName4);
+// var comnpanyName4="isim";
+// console.log(comnpanyName4);
 
-var _comnpanyName="isim";  //underscore
-console.log(_comnpanyName);
+// var _comnpanyName="isim";  //underscore
+// console.log(_comnpanyName);
 
-var $_comnpanyName="isim";  
-console.log($_comnpanyName);
+// var $_comnpanyName="isim";  
+// console.log($_comnpanyName);
 
 //let
-let companyName2="hello world";
-console.log(companyName2);
-console.warn(companyName2);
+// let companyName2="hello world";
+// console.log(companyName2);
+// console.warn(companyName2);
 
 
 //const
-const comnpanyName3="hello";
-//comnpanyName3="hello22";  değişmez
-console.error(comnpanyName3);
+// const comnpanyName3="hello";
+// //comnpanyName3="hello22";  değişmez
+// console.error(comnpanyName3);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// operator
+let data1=12;
+// let data2=7;
+
+// console.log(data1+data2);
+// console.log(data1-data2);
+// console.log(data1*data2);
+// console.log(data1/data2);
+
+// let result1=data1+data2;
+// console.log(result1);
+
+//karşılaştırma operatörleri
+// console.log(data1<data2);
+// console.log(data1>data2);
+// console.log(data1==data2);
+// console.log(data1!=data2);
+
+//atama operatörleri
+// const data33=10, data44="34";
+// console.log(data33==data44);
+//alert(data33===data44);   === içeriine ve türüne bakıyor
+ 
+//mantıksal operatörler
+// const data5=true, data6=false;
+// console.log(data5 && data6);   //false
+// console.log(data5 || data6);    //true
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//prompt
+// const userdata=prompt("lütfen adınızn giriniz ....");
+// alert(userdata);
+/////////////////////////////////////////////////////////////////////////////////////////
